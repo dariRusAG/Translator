7924ff57b95a57ca5c85e5c42b8a4bc51a2c095e
dariRusAG <agapova.dr@students.dvfu.ru>
2023-01-13 20:14:38 +1000

    Привдение в Main в адекватный вид после merge

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 import java.io.UnsupportedEncodingException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws UnsupportedEncodingException, Exception {
-
-//        String pascalFile = "src/code.pas";
-        String pascalFile = "src/program_1.pas";
-        // String pascalFile = "program1.txt";
-        PrintStream ps = new PrintStream(System.out, false, "utf-8");
+         String pascalFile = "program1.txt";
+//        PrintStream ps = new PrintStream(System.out, false, "utf-8");
         LexAnalyzer pascalLexAnal = new LexAnalyzer(pascalFile);
         try {
             pascalLexAnal.makeAnalysis();
@@ -16,36 +14,36 @@ public class Main {
             PascalGrammar pascalGrammar = new PascalGrammar(new Pair("nterm","программа"));
 //            pascalGrammar.print();
             CGrammar cGrammar = new CGrammar(new Pair("nterm", "программа"));
-//            cGrammar.print();
+            cGrammar.print();
+//            String pascalFile = "src/program_1.pas";
+//            PrintStream ps = new PrintStream(System.out, false, "utf-8");
             SynAnalyzer pascalSynAnal = new SynAnalyzer(pascalLexAnal.getListLexem(), pascalGrammar);
-            pascalSynAnal.makeTable();
-            pascalSynAnal.printTable();
+//            pascalSynAnal.makeTable();
+//            pascalSynAnal.printTable();
 
-//            pascalSynAnal.parse();
+            pascalSynAnal.parse();
 
             //30,31,6,4,39,16,14,12,38,35,1,0
-//            ArrayList<Integer> arrInt = new ArrayList<>();
-//            arrInt.add(30);
-//            arrInt.add(31);
-//            arrInt.add(6);
-//            arrInt.add(4);
-//            arrInt.add(39);
-//            arrInt.add(16);
-//            arrInt.add(14);
-//            arrInt.add(12);
-//            arrInt.add(38);
-//            arrInt.add(35);
-//            arrInt.add(1);
-//            arrInt.add(0);
-//            //ParseTree tree = pascalSynAnal.buildTree(arrInt);
-//
-//            arrInt.add(0);
+            ArrayList<Integer> arrInt = new ArrayList<>();
+            arrInt.add(30);
+            arrInt.add(31);
+            arrInt.add(6);
+            arrInt.add(4);
+            arrInt.add(39);
+            arrInt.add(16);
+            arrInt.add(14);
+            arrInt.add(12);
+            arrInt.add(38);
+            arrInt.add(35);
+            arrInt.add(1);
+            arrInt.add(0);
+//            ParseTree tree = pascalSynAnal.buildTree(arrInt);
+
+            arrInt.add(0);
 
         } catch (Exception e) {
             PrintStream ps = new PrintStream(System.out, false, "cp1251");
             ps.print(e.getMessage());
         }
-
-
     }
 }
